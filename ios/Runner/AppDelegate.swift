1fc4cc2437a89a6ef63baa492b43d795268018fb
mimo <mimoriam420@gmail.com>
2025-08-26 15:38:47 +0500

    preparation for ios 4

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import Flutter
 import UIKit
-import flutter_local_notifications
+// import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,9 +8,9 @@ import flutter_local_notifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-          GeneratedPluginRegistrant.register(with: registry)
-    }
+//     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+//           GeneratedPluginRegistrant.register(with: registry)
+//     }
     if #available(iOS 10.0, *) {
           UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
